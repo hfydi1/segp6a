fe8b2a612515a18cba2a4f50d41ca44557a12235
hfydi1 <hfydi1@nottingham.edu.my>
2020-11-21 22:58:27 +0800

    Added mouse input and keyboard input for player movement. Also added lights to prototyping game area.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 2f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; //0.4f is radius of sphere
+    public LayerMask groundMask; //to control what the groundCheck sphere will check for
+
+    Vector3 velocity; //stores player current velocity
+
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //walking
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //jumping
+        // v = sqrt(h*-2*g)
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //gravity
+
+        velocity.y += gravity * Time.deltaTime; //Time.deltaTime takes native framerate of player
+
+        controller.Move(velocity * Time.deltaTime); //multiply by Time again according to physics of a free fall
+
+    }
+}
