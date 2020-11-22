3704c093da421b6be6a5110ddeb6246fe54e36c7
hfydi1 <hfydi1@nottingham.edu.my>
2020-11-22 23:08:09 +0800

    Added RayCasting for player to interact with gameObjects.
    Added tables to prototyping stage.

---
+++ Assets/Scripts/KeyCards.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCards : MonoBehaviour
+{
+    public enum Keycards
+    { //define the types of keycards
+        redKey, blueKey, greenKey
+    }
+
+    public Keycards whatKeyAmI; //identifier
+}
---
+++ Assets/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    public float distanceToSee;
+    RaycastHit whatIHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+        if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
+        {
+            //Debug.Log("I touched "+ whatIHit.collider.gameObject.name);
+            if(Input.GetKeyDown(KeyCode.E)) //upon press E key
+            {
+                Debug.Log("I picked up a" + whatIHit.collider.gameObject.name);
+                if (whatIHit.collider.tag == "Keycards")
+                {
+                    if (whatIHit.collider.gameObject.GetComponent<KeyCards>().whatKeyAmI == KeyCards.Keycards.redKey)
+                    {
+                        Destroy (whatIHit.collider.gameObject);
+                    }
+                }
+            }
+        }
+    }
+}
