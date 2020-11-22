ddd51dc1f60a922cabde48f6a8a35e2f0d5ccb06
hfydi1 <hfydi1@nottingham.edu.my>
2020-11-23 03:04:00 +0800

    Added pause menu and paused game state.
    Known bugs:
    - Resume button not working

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            activateMenu();
+        }
+
+        else
+        {
+            deactivateMenu();
+        }
+    }
+
+    void activateMenu()
+    {
+        Time.timeScale = 0; //stop game
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void deactivateMenu()
+    {
+        Time.timeScale = 1; //resume game
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+}
